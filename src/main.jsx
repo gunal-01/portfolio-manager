ee01c8dabe7fbdd65efa10416f5216ef5cf29e3e
gunal-01 <gunalprakash006@gmail.com>
2025-01-17 13:39:49 +0530

    initialize portfolio-manager-project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Dashboard from './components/Dashboard'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Dashboard />
+  </StrictMode>,
+)
